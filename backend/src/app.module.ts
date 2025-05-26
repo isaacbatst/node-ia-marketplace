b35216abcc2d5dc26947696995a4144da7e5f384
Isaac Batista <isaacbatst@gmail.com>
2025-05-25 21:48:53 -0300

    feat: catalog backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CatalogModule } from './catalog/catalog.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CatalogModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
