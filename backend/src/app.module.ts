d6246326453466634dd62af49812b672b9904c72
Isaac Batista <isaacbatst@gmail.com>
2025-07-01 00:38:19 -0300

    feat: aula 1 create chat session

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CatalogModule } from './catalog/catalog.module';
 import { CartModule } from './cart/cart.module';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { CartModule } from './cart/cart.module';
     }),
     CatalogModule,
     CartModule,
+    ChatModule,
   ],
   controllers: [],
   providers: [],
